e871b707af02a0849e3d5c9c182c1801feb39e79
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-08-08 23:15:58 +0600

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+// state 
+const initialConterState = {
+    count : 0
+};
+const initialUsersState = {
+    users: [{ name: "anisul Islam"}]
+};
+
+
+
+
+// action
+// INCREMENT COUNTER
+{
+    type: 'INCREMENT',
+    
+}
